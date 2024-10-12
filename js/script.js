fa411317eac0af7244e0c58b8a6ba0e5359ad909
ferrari212 <felipe.ferrari.212@gmail.com>
2024-10-12 16:51:26 +0200

    Modifying the script.js base

--- js/script.js
+++ js/script.js
@@ -1,54 +1,63 @@
-import { HullHydrostatics } from "../source/jsm/physics/Hydrostatic.js";
 import * as Vessel3D from "../source/vessel3D.js";
+
+import { HullHydrostatics as Hydrostatic } from "../build/vessel3D.js";
 
 const ship = new Vessel3D.Ship()
 
+// Commands to create compartments
 // ship.addCompartments()
+
+
+
+// 1- Define the hull and add it under ship
+const hull = {}
+hull.halfBreadths = {        
+    "waterlines": [0, 0, 0.5, 1],
+    "stations":[0, 1],
+    "table": [[0 , 0],[1 , 1], [1 , 1],[1 , 1]],
+}
+
+hull.attributes = {
+    "LOA": 22.5,
+    "BOA": 10,
+    "Depth": 5,
+    "APP": 0
+}
+hull.style = {
+    "upperColor": "yellow",
+    "lowerColor": "green",
+    "opacity": 0.5
+}
+hull.design_draft = 3
+
+ship.addHull(hull)
 // ship.addCompartments({height: 20, xpos:0})
-// ship.addCompartments({length: 20, xpos:10})
+ship.addCompartments({length: 20, xpos:10})
 // ship.addCompartments({width: 20, xpos:10})
 
+// 2 - Create a scene
+const scene = new Vessel3D.Scene()
+scene.addShip( ship )
+scene.addAxesHelper()
 
-
-ship.addHull()
-
-// const hull = {}
-
-// hull.halfBreadths = {        
-//     "waterlines": [0, 0, 1],
-//     "stations":[0, 1],
-//     "table": [[0 , 0],[1 , 1],[1 , 1]],
-// }
-
-// hull.attributes = {
-//     "LOA": 22.5,
-//     "BOA": 10,
-//     "Depth": 2.5,
-//     "APP": 0
-// }
-// hull.style = {
-//     "upperColor": "yellow",
-//     "lowerColor": "green",
-//     "opacity": 0.5
-// }
-// ship.addHull(hull, 5)
-
-console.log(ship);
-
-ship.scene.addAxesHelper()
-
+// 3 - Initialize hydrostatic
+// The hydrostatic can be created using the initialize hydrostatic command or can
+// be created independently
 ship.initializeHydrostatics()
+const hydrostatics = new Hydrostatic(hull, 3)
 
 // Initialize dragControls
-// ship.initializeDragControls()
+scene.initializeDragControls()
 
-const scene = ship.scene
+console.log(ship);
+console.log(hydrostatics);
+
 
 // Render loop
 function animate() {
+
     requestAnimationFrame(animate);
-
-
     scene.renderer.render(scene, scene.camera);
+
 }
 animate();
