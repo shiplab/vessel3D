548ab3079da055be3f576018987c1d084a26e161
ferrari212 <felipe.ferrari.212@gmail.com>
2024-12-08 08:40:07 +0100

    adding the attributes configuration

--- source/jsm/ship/Ship.js
+++ source/jsm/ship/Ship.js
@@ -1,105 +1,90 @@
-import { Compartments } from "./Compartments.js";
-import { Hull } from "./Hull.js"
+import {Compartments} from "./Compartments.js";
+import {Hull} from "./Hull.js";
 
-import { HullHydrostatics } from "../physics/Hydrostatic.js"
+import {HullHydrostatics} from "../physics/Hydrostatic.js";
 
-import { PREDEFINED_HULLS } from "../database/predefinedHull.js"
+import {PREDEFINED_HULLS} from "../database/predefinedHull.js";
 
 export class Ship {
-
-    constructor( specification ) {
-
+    constructor(specification) {
         // TODO: Read the GLTF in case the specification is assigned
         // function read_gltf() {}
 
         // List of compartments
-        this.compartments = []
-        this.cargoCompartments = []
-        this.ballastCompartments = []
-        this.fuelCompartments = []
-        this.freshWater = []
-        
+        this.compartments = [];
+        this.cargoCompartments = [];
+        this.ballastCompartments = [];
+        this.fuelCompartments = [];
+        this.freshWater = [];
+
         // List of equipments
-        this.engine = []
-        this.equipments = []
-
+        this.engine = [];
+        this.equipments = [];
     }
 
-    addHull (hull = undefined, design_draft = undefined) {
+    addHull(hull = undefined, att = {}) {
+        const {design_draft = undefined, predefinedHullName = undefined} = att;
 
-        if(hull === undefined) {
+        if (hull === undefined) {
             // Undefined hull will be assigned automatically to Wigley Hull
-
-            const HULL = PREDEFINED_HULLS["wigleyHull"]
-            this.hull = new Hull(HULL)
-
-            return this.hull
-            
+            return this.getPredefinedHull(predefinedHullName);
         }
-        
-        if(hull.hasOwnProperty('design_draft') && typeof hull.design_draft !== "number") {
-            
-            // Assign the design draft written in the hull object 
-            design_draft = hull.design_draft
-        
-        } 
 
-        this.hull = new Hull(hull, design_draft)
+        if (hull.hasOwnProperty("design_draft") && typeof hull.design_draft !== "number") {
+            // Assign the design draft written in the hull object
+            design_draft = hull.design_draft;
+        }
 
-        return this.hull
+        this.hull = new Hull(hull, design_draft);
 
+        return this.hull;
     }
 
-    initializeHydrostatics () {
-
-        if ( !this.hull ) throw new Error( 'Hydrostatics only available after hull definition. Use addHull method' );
-
-        this.HullHydrostatics = new HullHydrostatics(this.hull, this.hull.design_draft)
-
+    getPredefinedHull(hullName = "wigleyHull") {
+        // Add a predefined hull, options are:
+        // barge, wigleyHull
+        if (!["barge", "wigleyHull"].includes(hullName)) {
+            throw new Error(`Predefined hullName = ${hullName} not defined in the list of predefined ships.`);
+        }
+        return new Hull(PREDEFINED_HULLS[hullName]);
     }
 
-    initializeStability () {
-        
-        if ( this.HullHydrostatics ) throw new Error( "Hydrostatics is a subclass of Stability. Therefore, no need for initializing hydrostatics 'shi.initializeHydrostatics()'." );
+    initializeHydrostatics() {
+        if (!this.hull) throw new Error("Hydrostatics only available after hull definition. Use addHull method");
 
-        this.HullStability = new HullStability(this.ship)
-
+        this.HullHydrostatics = new HullHydrostatics(this.hull, this.hull.design_draft);
     }
 
-    addBulkhead (xpos_aft, thickness, density) {
+    initializeStability() {
+        if (this.HullHydrostatics)
+            throw new Error(
+                "Hydrostatics is a subclass of Stability. Therefore, no need for initializing hydrostatics 'shi.initializeHydrostatics()'."
+            );
 
+        this.HullStability = new HullStability(this.ship);
+    }
+
+    addBulkhead(xpos_aft, thickness, density) {
         try {
-            
             if (this.hull === undefined) {
-                
                 // Throw an error if "hull" is not defined
                 throw new Error("The key 'hull' is not defined in the object. Use the addHull function to insert a hull first");
-
             }
-
         } catch (error) {
-
             // Throw an error if "hull" is not defined
             throw new Error("The key 'hull' is not defined in the object.");
-            
         }
 
-        this.hull.addBulkheads(this.hull.attributes, xpos_aft, thickness, density)
-        
+        this.hull.addBulkheads(this.hull.attributes, xpos_aft, thickness, density);
     }
 
     addCompartments(compartmentObject) {
-    
-        const compartment = new Compartments(compartmentObject)
+        const compartment = new Compartments(compartmentObject);
 
-        this.compartments.push(compartment)
-        
+        this.compartments.push(compartment);
     }
 
     initializeDragControls() {
-        
-        this.scene._initializeDragControls(this.compartments)
-
+        this.scene._initializeDragControls(this.compartments);
     }
-
 }
